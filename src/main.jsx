043978069bccbe807e1e5fffcbcfde540e78024d
Nischal <nischal.maharjan1233@gmail.com>
2024-06-20 21:53:08 +0545

    viteconfig file

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import "@fontsource/outfit"
-import "@fontsource/roboto"
+import "@fontsource/outfit";
+import "@fontsource/roboto";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@fontsource/outfit'],
+  },
   css:{
     modules:{
       localsConvention:"camelCase",
     }
   }
+  
 })
